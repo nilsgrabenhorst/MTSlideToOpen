c27c34c20f2723537800e4b9cd119707c4dc6880
Nils Grabenhorst <nils.grabenhorst@sinnerschrader.com>
2023-08-02 21:05:50 +0200

    Add package manifest

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MTSlideToOpen",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        .library(
+            name: "MTSlideToOpen",
+            targets: ["MTSlideToOpen"]),
+    ],
+    targets: [
+        .target(
+            name: "MTSlideToOpen"),
+    ]
+)
